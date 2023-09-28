1b7f9496e6a94e34c7d52a0c4ac0d35b2cf416fd
halohoang <62345058+halohoang@users.noreply.github.com>
2023-09-28 16:34:28 +0200

    Implement max and min speed

--- Assets/Scripts/Player/Movement.cs
+++ Assets/Scripts/Player/Movement.cs
@@ -7,7 +7,10 @@ public class Movement : MonoBehaviour
 {
 	// Variables
 	/* Player Movement Variables */
-	[SerializeField] private float _playerSpeed; //How fast the player move
+	[SerializeField] private float _currentPlayerSpeed; //How fast the player move
+	[SerializeField] private float _minPlayerSpeed; //slowest the player move
+	[SerializeField] private float _maxPlayerSpeed; //fastest the player move
+
 	[SerializeField] private float _playerDashForce;
 	internal bool _isMoving;
 
@@ -15,19 +18,20 @@ public class Movement : MonoBehaviour
 	//Functions
 	void Start()
 	{
-
+		_currentPlayerSpeed = _minPlayerSpeed;
 	}
 
 	void Update()
 	{
 		PlayerMovement();
-		//PlayerDash();
+		PlayerFast();
+		PlayerDash();
 
 	}
 
 
 
-	/*Player Movement*/
+	/* Player Movement*/
 	void PlayerMovement()
 	{
 
@@ -35,22 +39,22 @@ public class Movement : MonoBehaviour
 		if (Input.GetKey(KeyCode.A))
 		{
 			_isMoving = true;
-			transform.Translate(Vector2.left * _playerSpeed * Time.deltaTime, Space.World);
+			transform.Translate(Vector2.left * _currentPlayerSpeed * Time.deltaTime, Space.World);
 		}
 		if (Input.GetKey(KeyCode.D))
 		{
-			transform.Translate(Vector2.right * _playerSpeed * Time.deltaTime, Space.World);
+			transform.Translate(Vector2.right * _currentPlayerSpeed * Time.deltaTime, Space.World);
 		}
 
 		if (Input.GetKey(KeyCode.W))
 		{
 			_isMoving = true;
-			transform.Translate(Vector2.up * _playerSpeed * Time.deltaTime, Space.World);
+			transform.Translate(Vector2.up * _currentPlayerSpeed * Time.deltaTime, Space.World);
 		}
 		if (Input.GetKey(KeyCode.S))
 		{
 			_isMoving = true;
-			transform.Translate(Vector2.down * _playerSpeed * Time.deltaTime, Space.World);
+			transform.Translate(Vector2.down * _currentPlayerSpeed * Time.deltaTime, Space.World);
 		}
 		// Check if none of the movement keys are pressed
 		if (!Input.GetKey(KeyCode.A) && !Input.GetKey(KeyCode.D) && !Input.GetKey(KeyCode.W) && !Input.GetKey(KeyCode.S))
@@ -58,49 +62,61 @@ public class Movement : MonoBehaviour
 			_isMoving = false;
 		}
 	}
+
+	/* Player Sandevistan */
+	void PlayerFast()
+	{
+		//Player move faster
+		if (Input.GetKeyDown(KeyCode.Space))
+		{
+			_currentPlayerSpeed = _maxPlayerSpeed;
+		} 
+		if (!Input.GetKey(KeyCode.Space)) { _currentPlayerSpeed = _minPlayerSpeed; } //Revert player back to normal speed
+	}
+
 	/* Player Dash */
-	//void PlayerDash()
-	//{
-	//	if (Input.GetKey(KeyCode.A) && Input.GetKeyDown(KeyCode.Space))
-	//	{
-	//		/*Dash direction to the left */
-	//		Vector2 dashDirection = Vector2.left;
-	//		/* Apply Dash Force */
-	//		Vector2 dashForce = dashDirection * _playerDashForce * Time.deltaTime;
-	//		transform.Translate(dashForce, Space.World);
-	//		_isMoving = true;
-	//	}
-	//	if (Input.GetKey(KeyCode.D) && Input.GetKeyDown(KeyCode.Space))
-	//	{
-	//		/*Dash direction to the left */
-	//		Vector2 dashDirection = Vector2.right;
-	//		/* Apply Dash Force */
-	//		Vector2 dashForce = dashDirection * _playerDashForce * Time.deltaTime;
-	//		transform.Translate(dashForce, Space.World);
-	//		_isMoving = true;
+	void PlayerDash()
+	{
+		if (Input.GetKey(KeyCode.A) && Input.GetKeyDown(KeyCode.LeftShift))
+		{
+			/*Dash direction to the left */
+			Vector2 dashDirection = Vector2.left;
+			/* Apply Dash Force */
+			Vector2 dashForce = dashDirection * _playerDashForce * Time.deltaTime;
+			transform.Translate(dashForce, Space.World);
+			_isMoving = true;
+		}
+		if (Input.GetKey(KeyCode.D) && Input.GetKeyDown(KeyCode.LeftShift))
+		{
+			/*Dash direction to the left */
+			Vector2 dashDirection = Vector2.right;
+			/* Apply Dash Force */
+			Vector2 dashForce = dashDirection * _playerDashForce * Time.deltaTime;
+			transform.Translate(dashForce, Space.World);
+			_isMoving = true;
 
-	//	}
-	//	if (Input.GetKey(KeyCode.W) && Input.GetKeyDown(KeyCode.Space))
-	//	{
-	//		/*Dash direction to the left */
-	//		Vector2 dashDirection = Vector2.up;
-	//		/* Apply Dash Force */
-	//		Vector2 dashForce = dashDirection * _playerDashForce * 0.5f * Time.deltaTime;
-	//		transform.Translate(dashForce, Space.World);
-	//		_isMoving = true;
+		}
+		if (Input.GetKey(KeyCode.W) && Input.GetKeyDown(KeyCode.LeftShift))
+		{
+			/*Dash direction to the left */
+			Vector2 dashDirection = Vector2.up;
+			/* Apply Dash Force */
+			Vector2 dashForce = dashDirection * _playerDashForce * 0.5f * Time.deltaTime;
+			transform.Translate(dashForce, Space.World);
+			_isMoving = true;
 
-	//	}
+		}
 
-	//	if (Input.GetKey(KeyCode.S) && Input.GetKeyDown(KeyCode.Space))
-	//	{
-	//		/*Dash direction to the left */
-	//		Vector2 dashDirection = Vector2.down;
-	//		/* Apply Dash Force */
-	//		Vector2 dashForce = dashDirection * _playerDashForce * 0.5f * Time.deltaTime;
-	//		transform.Translate(dashForce, Space.World);
-	//		_isMoving = true;
+		if (Input.GetKey(KeyCode.S) && Input.GetKeyDown(KeyCode.LeftShift))
+		{
+			/*Dash direction to the left */
+			Vector2 dashDirection = Vector2.down;
+			/* Apply Dash Force */
+			Vector2 dashForce = dashDirection * _playerDashForce * 0.5f * Time.deltaTime;
+			transform.Translate(dashForce, Space.World);
+			_isMoving = true;
 
-	//	}
-	//}
+		}
+	}
 }
 
