0793ae76201a2e5eb69d0ed824c5087333179bea
halohoang <62345058+halohoang@users.noreply.github.com>
2023-10-01 16:40:58 +0200

    Bar now depleted
    Somebody pls help me calibrate the rate at which the bar fill up

--- Assets/Scripts/Player/PlayerHealth.cs
+++ Assets/Scripts/Player/PlayerHealth.cs
@@ -8,6 +8,8 @@ public class PlayerHealth : MonoBehaviour
 	[SerializeField] private float _maxHealth;
 	[SerializeField] internal float _currentHealth;
 	[SerializeField] GameObject _player;
+	[SerializeField] Transform _chargeBarTransform; // Reference to the scale of the bar
+	[SerializeField] float _chargeSpeed = 0.1f; // The rate at which bar depletes or charges
 
 	private Rigidbody2D _rb2D;
 
@@ -36,6 +38,7 @@ public class PlayerHealth : MonoBehaviour
 			if (Input.GetKey(KeyCode.Space))
 			{
 				_currentHealth -= 0.5f;
+				ReduceCharge();
 			}
 
 			// Ensure _currentHealth does not go below 0
@@ -46,10 +49,31 @@ public class PlayerHealth : MonoBehaviour
 			if (!Input.GetKey(KeyCode.Space) && _currentHealth != _maxHealth)
 			{
 				_currentHealth += 0.1f;
+				RegenCharge();
 			}
 
 			_currentHealth = Mathf.Clamp(_currentHealth, 0f, _maxHealth);
 
 		}
 	}
+
+	void ReduceCharge()
+	{
+		if (_chargeBarTransform != null)
+		{
+			Debug.Log("reduce");
+			_chargeBarTransform.localScale -= new Vector3(0f, _chargeSpeed, 0f) * Time.deltaTime; // Reduce the Y scale of the charge bar by chargeSpeed
+			_chargeBarTransform.localScale = new Vector3(_chargeBarTransform.localScale.x, Mathf.Max(0f, _chargeBarTransform.localScale.y), _chargeBarTransform.localScale.z); // Make sure scale does not go below 0
+		}
+	}
+	void RegenCharge()
+	{
+		if (_chargeBarTransform != null)
+		{
+			_chargeBarTransform.localScale += new Vector3(0f, _chargeSpeed * 0.3f, 0f) * Time.deltaTime; // Increase the Y scale of the charge bar by chargeSpeed
+			_chargeBarTransform.localScale = new Vector3(_chargeBarTransform.localScale.x, Mathf.Min(_chargeBarTransform.localScale.y, 0.16f), _chargeBarTransform.localScale.z); // Make sure scale does not go above 0
+
+		}
+	}
+
 }
