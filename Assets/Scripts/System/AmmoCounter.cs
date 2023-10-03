ca20c57a68ef93a6988555665a742fccbd5a5db9
halohoang <62345058+halohoang@users.noreply.github.com>
2023-10-03 16:46:40 +0200

    Implement Prototype for Ammo Counter UI

--- Assets/Scripts/System/AmmoCounter.cs
+++ Assets/Scripts/System/AmmoCounter.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AnimationSpeedUp : MonoBehaviour
+public class AmmoCounter : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
