e445a9577ff8e4abd4aa29951c7304929cea99c6
JMindbender <jp.mak@outlook.com>
2023-11-16 15:26:30 +0100

    Bugfixes and added WinningScene Trigger Zone
    - Disabled Player/MouseRotation on PlayerDeath
    - Melee Enemies will now go to last known Player Position when loosing sight of him
    - Added a TriggerZone in the second Elevator that will fire an event in OnTriggerEnter2D()

--- Assets/Scripts/Player/RotatePlayertoMouse.cs
+++ Assets/Scripts/Player/RotatePlayertoMouse.cs
@@ -1,30 +1,47 @@
-using System.Collections;
-using System.Collections.Generic;
+using NaughtyAttributes;
 using UnityEngine;
 
 public class RotatePlayertoMouse : MonoBehaviour
 {
-	// Variables
-	private Rigidbody2D _rb2D;
-	[SerializeField] private Transform _playerTransform; // Reference to the player's transform.
+    // Variables
+    private Rigidbody2D _rb2D;
+    [SerializeField] private Transform _playerTransform; // Reference to the player's transform.	
+    [SerializeField, ReadOnly] private bool _isPlayerDead;
 
-	void Start()
-	{
-		_rb2D = GetComponent<Rigidbody2D>();
-		/*_playerTransform = transform.parent;*/ // Get the player's transform from the parent.
-	}
+    private void OnEnable()
+    {
+        PlayerHealth.OnPlayerDeath += SetIsPlayerDead;
+    }
+    private void OnDisable()
+    {
+        PlayerHealth.OnPlayerDeath -= SetIsPlayerDead;
+    }
 
-	//Functions
-	void Update()
-	{
-		RotateToMousePosition();
-	}
+    //Functions
+    void Update()
+    {
+        RotateToMousePosition();
+    }
 
-	void RotateToMousePosition()
-	{
-		Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-		Vector2 direction = (mousePos - _playerTransform.position).normalized; // Use player's position.
-		float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
-		_rb2D.rotation = angle;
-	}
+    void RotateToMousePosition()
+    {
+        if (!_isPlayerDead)
+        {
+            Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Vector2 direction = (mousePos - _playerTransform.position).normalized; // Use player's position.
+            float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+            _rb2D.rotation = angle;
+        }
+    }
+
+    private void SetIsPlayerDead(bool isPlayerDead)
+    {
+        _isPlayerDead = isPlayerDead;
+    }
+
+    void Start()
+    {
+        _rb2D = GetComponent<Rigidbody2D>();
+        /*_playerTransform = transform.parent;*/ // Get the player's transform from the parent.
+    }
 }
