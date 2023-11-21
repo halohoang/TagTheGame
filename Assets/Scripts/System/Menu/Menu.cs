3e6370a4c5ece4ac01f70f69288fa4bb88bcf04c
halohoang <62345058+halohoang@users.noreply.github.com>
2023-11-21 17:18:04 +0100

    Prototype of Letters

--- Assets/Scripts/System/Menu/Menu.cs
+++ Assets/Scripts/System/Menu/Menu.cs
@@ -5,19 +5,23 @@ using UnityEngine.SceneManagement;
 public class Menu : MonoBehaviour
 {
 	// Variables
-	[SerializeField] AudioSource _AudioSource;
+	[SerializeField] AudioSource _audioSource;
 	[SerializeField] AudioClip _AudioClip;
-	
+	float _startVolume;
+	[SerializeField] float _fadeDuration = 2.0f;
+
+
 	internal bool _startPlay;
 	// Functions
 	private void Start()
 	{
-		_AudioSource = GetComponent<AudioSource>();
+		_audioSource = GetComponent<AudioSource>();
+		_startVolume = _audioSource.volume;
 		
 	}
 	public void Play()
 	{
-		_AudioSource.PlayOneShot(_AudioClip,1);
+		_audioSource.PlayOneShot(_AudioClip,1);
 		_startPlay = true;
 		
 		StartCoroutine(Delay(1, 3.1f)); //Load scene 1 after 3.1s delay
@@ -31,5 +35,14 @@ public class Menu : MonoBehaviour
 	{
 		yield return new WaitForSeconds(delay);
 		SceneManager.LoadScene(sceneIndex);
+	}
+
+	public void BackToMainMenu()
+	{
+		StartCoroutine(Delay(0, 1f)); //Load scene 2 after 1.5s delay
+	}
+	public void Letters() // A letter to a very single one
+	{
+		StartCoroutine(Delay(3, 3.1f)); //Load scene 2 after 1.5s delay
 	}
 }
