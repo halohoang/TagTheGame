c10df53c4ec5a8958598f4f5d8416370a47d56b6
JMindbender <jp.mak@outlook.com>
2024-01-12 15:37:26 +0100

    just a 'chache' for saving minor changes before merge

--- Assets/Testing/Jan/Scripts/Player/PlayerEquipmentSO.cs
+++ Assets/Testing/Jan/Scripts/Player/PlayerEquipmentSO.cs
@@ -1,9 +1,17 @@
+using NaughtyAttributes;
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace ScriptableObjects
 {
+    [CreateAssetMenu(menuName = "Scriptable Objects/Player", fileName = "PlayerEquipment")]
     public class PlayerEquipmentSO : ScriptableObject
     {
+        // ---------- Fields ----------
+        //[SerializeField, ReadOnly] private List<>
+
+        
+        // ---------- Methods ----------
         
     }
 }
